5251a5f765a020874c7afad85682055ceb7b8944
Anthony Tesche <tescheanthony@gmail.com>
2023-07-25 23:02:34 -0300

    :sparkles: initial commit

---
+++ Block/Tags.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tesche\MetaTags\Block;
+
+use \Magento\Framework\View\Element\Template;
+use \Magento\Framework\View\Element\Template\Context;
+use \Magento\Cms\Model\Page;
+use \Magento\Framework\Locale\ResolverInterface;
+use \Magento\Store\Model\StoreManagerInterface;
+
+class Tags extends Template
+{
+    /**
+     * @var Page
+     */
+    public $page;
+
+    /**
+     * @var ResolverInterface
+     */
+    public $resolverInterface;
+
+    public function __construct(
+        Context $context,
+        Page $page,
+        ResolverInterface $resolverInterface,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->page = $page;
+        $this->resolverInterface = $resolverInterface;
+        $this->storeManager = $storeManager;
+        parent::__construct($context);
+    }
+
+    public function showTags(): bool
+    {
+        if (is_array($this->page->getStoreId())) {
+            if (
+                count($this->page->getStoreId()) > 1 ||
+                (
+                    isset($this->page->getStoreId()[0]) &&
+                    $this->page->getStoreId()[0] === 0
+                )
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getStoreLanguage()
+    {
+        return strtolower(
+            str_replace('_', '-', $this->resolverInterface->getLocale())
+        );
+    }
+
+    public function getTagHref()
+    {
+        return $this->storeManager->getStore()->getBaseUrl() . $this->page->getIdentifier();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# M2-Module-MetaTags
+# Meta Tags - Module for Magento 2
+
+IMPORTANT: IT'S JUST A TEST MODULE, THERE WILL BE NO MAINTENANCE IN THE FUTURE
+
+## Description
+The module adds a <link> tag to pages used in multiple store views.
+
+The `<link>` tag is added inside the `<head>` tag
+
+Example tag:
+```html
+<link rel="alternate" hreflang="en-us" href="https://magento.test/no-route">
+```
+
+## Installation
+
+The extension can be installed via `composer`. To proceed, run these commands in your terminal:
+
+```
+composer require tesche/rangeproducts
+php bin/magento module:enable Tesche_MetaTags
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy
+```
+
+## How to use
+The module after enabled will work and show the tag on pages that have use in multiple views automatically
